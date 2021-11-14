8ba578282410712dc66c7982b5e7f7d28b053428
Stefanie <stefanie.vicente@hotmail.com>
2021-11-14 14:24:02 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import axios from 'axios';
+import LoadingButton from '@mui/lab/LoadingButton';
+
 function App() {
+  const [joke, setJoke] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const buttonClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setLoading(true)
+    axios
+      .get('https://dadjoke-flask-api.herokuapp.com/dadjoke')
+      .then(({data}) => {
+        setJoke(data.joke)
+        setLoading(false)
+      }) 
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>{joke}</p>
+        <LoadingButton loading={loading} variant="outlined" onClick={event => buttonClickHandler(event)}>
+          Dad Joke
+        </LoadingButton>      
+      </div>
     </div>
   );
 }
